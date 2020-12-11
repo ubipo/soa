7725eb95dc3c2a659441d9e4e19b5c1ce90d3ec9
Pieter Fiers <pieter@pfiers.net>
2020-12-12 00:53:44 +0100

    Removed vue-select

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,12 @@ import 'regenerator-runtime/runtime'
 
 import { createApp } from 'vue';
 //@ts-ignore
-import VueSelect from "vue-select";
 import createAppRouter from './router/router';
 
 import Main from './view/components/Main.vue'
 
-
 const router = createAppRouter()
 const app = createApp(Main, { router })
 app.use(router)
-app.component("VueSelect", VueSelect)
 
 app.mount('#app')
